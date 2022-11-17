0245e2fdc4b5e8d251c2f9b7c12f815c8ab42a2b
Alberto Cabrera <albertocabja@gmail.com>
2022-11-17 15:21:08 -0600

    Initialized Android project and implemented a BottomNavigationBar with binding

---
+++ app/src/main/java/al10101/android/flinder/MainActivity.kt
@@ -0,0 +1,36 @@
+package al10101.android.flinder
+
+import al10101.android.flinder.databinding.ActivityMainBinding
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val bottomNavigationView = binding.bottomNavigationView
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
+
+        // To change the name in the App  Bar according to the Fragment
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.fragmentSavedImages, R.id.fragmentFlickr)
+        )
+
+        val navController = navHostFragment.navController
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        bottomNavigationView.setupWithNavController(navController)
+
+    }
+
+}
